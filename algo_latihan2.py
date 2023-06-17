c828801cfdf55e2d5a6cbda5711601f32686884a
WithMyGitHub Pemrograman <136155357+m-adin-informatika@users.noreply.github.com>
2023-06-17 09:17:58 +0700

    Update algo_latihan2.py
    Mengisi kode python algo_latihan2

--- algo_latihan2.py
+++ algo_latihan2.py
@@ -1,1 +1,34 @@
+# ALGO_LATIHAN2
 
+# VARIABEL MASUKAN
+a = 10
+b = 20
+c = 50
+
+# MENGHITUNG NILAI DARI (a+b)*(b*c)/(a+b+c)
+hasil1 = (a+b)*(b*c)/(a+b+c)
+print("-----------------------------------------------")
+print("HASIL NILAI (a+b)*(b*c)/(a+b+c)")
+print(hasil1)
+print("-----------------------------------------------")
+
+# MENGHITUNG NILAI DARI P * L * t
+hasil2 = a * b * c
+print("-----------------------------------------------")
+print("HASIL NILAI P * L * t")
+print(hasil2)
+print("-----------------------------------------------")
+
+# MENGHITUNG NILAI DARI 1/2 * a * t
+hasil3 = 0.5 * a * c
+print("-----------------------------------------------")
+print("HASIL NILAI dari 1/2 * a * t")
+print(hasil3)
+print("-----------------------------------------------")
+
+# MENGHITUNG NILAI DARI ((a + b)^2 + (b + c)^2) / (a*b)
+hasil4 = (((a + b) ** 2) + ((b + c) ** 2)) / (a * b)
+print("-----------------------------------------------")
+print("HASIL NILAI ((a + b)^2 + (b + c)^2) / (a*b)")
+print(hasil4)
+print("-----------------------------------------------")
