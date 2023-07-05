3751eb06586d7f444e93093c88804c450be060c3
WithMyGitHub Pemrograman <136155357+m-adin-informatika@users.noreply.github.com>
2023-07-06 06:33:04 +0700

    Update algo_latihan6.py

--- algo_latihan6.py
+++ algo_latihan6.py
@@ -1,1 +1,48 @@
+# SETTING VARIABEL IDENTITAS
+nim   = input("Masukan nim : ")
+nama  = input("Masukan nama : ")
+kelas = input("Masukan kelas: ")
+prodi = input("Masukan prodi: ")
 
+jari_jari = int(input("Masukan jari-jari lingkaran :"))
+phi = 3.14
+
+# OPERASI VARIABEL
+luas_lingkaran = 3.14 * jari_jari * jari_jari
+
+# CETAK HASIL
+print("")
+print("=====================================")
+print("         OPERASI ARITMATIKA          ")
+print("=====================================")
+print("")
+print("Nim   : %s" %nim)
+print("Nama  : %s" %nama)
+print("Kelas : %s" %kelas)
+print("Prodi : %s" %prodi)
+print("")
+print("=====================================")
+print("           LUAS LINGKARAN            ")
+print("=====================================")
+print("")
+print("Jari-jari lingkaran : %d" %jari_jari)
+print("Luas lingkaran      : %d" %luas_lingkaran)
+print("")
+print("=====================================")
+print("")
+
+
+# RANGKAIAN KATA
+print("=====================================")
+print("           RANGKAIAN KATA            ")
+print("=====================================")
+print("")
+
+# RANGKAIAN KATA LIST
+rangkaikata = ["UNISS","di","belajar","pada","Mahasiswa","Pemrograan","ruang","lab","Algoritma","semester","topik","Data","Tipe","dan","dengan","Batang",2]
+
+print(rangkaikata[4],rangkaikata[0],rangkaikata[15],rangkaikata[9],rangkaikata[16],rangkaikata[2],rangkaikata[8],rangkaikata[13],rangkaikata[5],rangkaikata[14],rangkaikata[10],rangkaikata[12],rangkaikata[11],rangkaikata[1],rangkaikata[6],rangkaikata[7])
+
+print("")
+print("=====================================")
+print("")
