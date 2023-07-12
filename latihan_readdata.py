179885e90b9f76ae905d6b825104a4925110d623
WithMyGitHub Pemrograman <136155357+m-adin-informatika@users.noreply.github.com>
2023-07-12 14:36:32 +0700

    Update latihan_readdata.py

--- latihan_readdata.py
+++ latihan_readdata.py
@@ -1,1 +1,10 @@
-Program Latihan Read Data
+# MEMBUKA FILE DATA
+data = open("data_iris.txt","r")
+print("Nama File",data.name)
+
+# MEMBACA FILE DATA
+file_konten = data.read(100)
+print("Isi Data :",file_konten[17:39])
+
+# MENUTUPP FILE DATA
+data.close()
